232d08afe3a0dd60a55e6aeadd4b0b471d2d88ad
muyonghui <muyonghui1996@gmail.com>
2017-10-26 23:38:36 +0800

    手写练习排序算法

--- src/main/java/com/muyonghui/Main.java
+++ src/main/java/com/muyonghui/Main.java
@@ -1,72 +1,23 @@
-package com.muyonghui;/*
- * Created by muyonghui on 2017/9/10.
- */
+package com.muyonghui;
 
-import sun.nio.cs.ext.ISCII91;
-import sun.tools.jconsole.MaximizableInternalFrame;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.math.BigDecimal;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        List l = new ArrayList<int[]>();
-        for (int i = 0; i < a.length; i++) {
-            a[i] = sc.nextInt();
-        }
-        int count = 0;
-        int i=0;
-        while (i<a.length) {
-            if (a[i] == 0){
-                count++;
+        Long[] data = {1L,2L,3L,4L,5L,6L,7L,8L,9L,10L};
+        Long[] res = new Long[data.length];
+        for (int i = data.length-1; i >= 5;i--) {
+            BigDecimal sum = BigDecimal.ZERO;
+            for (int j = 0; j < 5; j++) {
+                sum = sum.add(new BigDecimal(data[i-j]));
             }
-            i++;
-            int tmp = i;
-            for (int j = i; j < a.length; j++) {
-                if (IsXor(a,tmp,j)){
-                    count++;
-                    int[] b = new int[2];
-                    b[0] = tmp;
-                    b[1] = j;
-                    l.add(b);
-                    tmp=j+1;
-                }
-            }
+            res[i-5] = sum.divide(new BigDecimal(5)).longValue();
+
         }
-        for (int j = 0; j < l.size(); j++) {
-
-            for (int k = j+1; k < l.size(); k++) {
-                int[] x = (int[]) l.get(j);
-                int[] y = (int[]) l.get(k);
-
-                if (isOverlap(x,y)){
-                    count--;
-                }
-            }
+        for (Long count :
+                res) {
+            System.out.println(count);
         }
-        System.out.println(count);
-
-    }
-
-    private static boolean isOverlap(int[] l1,int[] l2){
-        int start = Math.max(l1[0],l2[0]);
-        int end = Math.min(l1[1],l2[1]);
-        if(start-end>=0)
-            return true;
-        return false;
-    }
-    private static boolean IsXor(int[] number, int start, int end) {
-        int v = 0;
-        for (int i = start;i <= end;i++) {
-            v ^= number[i];
-        }
-
-        return (v == 0) ? true : false;
     }
 }
