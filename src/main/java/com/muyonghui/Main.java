84df1e8522b166ee5d088921ee2a7cea58e4ed77
muyonghui <muyonghui1996@gmail.com>
2018-03-12 12:41:34 +0800

    添加学习问题记录

--- src/main/java/com/muyonghui/Main.java
+++ src/main/java/com/muyonghui/Main.java
@@ -1,27 +1,62 @@
-package com.muyonghui;/*
- * Created by muyonghui on 2017/9/10.
- */
+package com.muyonghui;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-//        int i = 0;
-//        if (i){
-//            System.out.println(1);
+
+        Scanner in = new Scanner(System.in);
+//        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");//设置日期格式
+//        Date date = df.parse(sc.nextLine());
+//        Date now =df.parse("2015-10-18");
+//        long l=date.getTime()-now.getTime();
+//        long day=l/(24*60*60*1000);
+//        Math.pow(x,3)
+//        Math.abs(a);
+//        char[] ch = str.toCharArray();
+//        StringBuffer sb = new StringBuffer();
+//        char c = str1.charAt(i);
+//        if ((str2.indexOf(c)) == -1){
+        // 将字符串翻转
+//        public static String reverse(String str) {
+//            StringBuilder sb = new StringBuilder(str);
+//            return sb.reverse().toString();
 //        }
-        boolean b =true;
-        boolean b2 =true;
-        if (b == b2) {
-            System.out.println("so true");
-        }
-        int i =1;
-        int j =2;
-        if (i==1||j==2)
-            System.out.println("ok");
+//
+//        // 求两个字符串的最长公共子串
+//        public static int getSubstring(String str) {
+//            String str2 = reverse(str);
+//            int i = 0;
+//            int j = 0;
+//            int len = str.length();
+//            int dp[][] = new int[len + 1][len + 1];
+//            for (i = 1; i <= len; i++) {
+//                for (j = 1; j <= len; j++) {
+//                    if (str.charAt(i - 1) == str2.charAt(j - 1)) {
+//                        dp[i][j] = dp[i - 1][j - 1] + 1;
+//                    } else {
+//                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+//                    }
+//                }
+//            }
+//            return dp[len][len];
+//        }
+
+
+//        public static void stringReverse(String[] a) {
+//            int num=a.length-1;
+//            String temp=null;
+//            for(int i=0;i<a.length/2;i++){
+//                temp=a[i];
+//                a[i]=a[num-i];
+//                a[num-i]=temp;
+//            }
+//        }
 
     }
 
